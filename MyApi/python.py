1f18f96b087fa765d3b60fff3273d630281a44ce
yukesh570 <yukesh.maha10@gmail.com>
2024-06-12 16:02:39 +0545

    pointdetection

--- MyApi/python.py
+++ MyApi/python.py
@@ -6,9 +6,9 @@ from .views import start_timer
 # Define the lower and upper bounds for red color in HSV space
 
 
-lower_red1 = np.array([0, 170, 40])
-upper_red1 = np.array([10, 255, 255])
-lower_red2 = np.array([160, 170, 40])
+lower_red1 = np.array([160, 40, 145])
+upper_red1 = np.array([180, 255, 255])
+lower_red2 = np.array([160, 40, 145])
 upper_red2 = np.array([180, 255, 255])
 
 
@@ -62,9 +62,10 @@ def video_feed(request, pk):
             raise IOError("Video file cannot be opened.")
         
     skip_frames = 2
-    frame_count = 0
+    frame_count = 2
     gameStarted=False
-
+    count = 0 
+    
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
@@ -87,7 +88,7 @@ def video_feed(request, pk):
         # dst = cv2.fastNlMeansDenoisingColored(frame,None,15,15,3,10)
         red_mask=detectRedObjects(frame)
         mask=detectObjects(frame)
-        median_blur= cv2.medianBlur(red_mask,21)
+        median_blur= cv2.medianBlur(red_mask,23)
         median_blur_white= cv2.medianBlur(mask,21)
         # median_blur = median_blur.astype(np.uint8)
         # median_blur_white = median_blur_white.astype(np.uint8)
@@ -99,10 +100,64 @@ def video_feed(request, pk):
         contours, hierarchy = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         contours2, hierarchy = cv2.findContours(canny2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         frame_copy=frame.copy()
-        cv2.drawContours(frame,contours,-1,(255,0,0),5)
-        cv2.drawContours(frame_copy,contours2,-1,(255,0,0),10)
+       
+            
+        for contour in contours:
+            epsilon = 0.03 * cv2.arcLength(contour, True)
+            approxed = cv2.approxPolyDP(contour, epsilon, True)
+            approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
 
-        threshold_distance = 18
+            M = cv2.moments(approx)
+            if M["m00"] != 0:
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+
+                # Define position and size of the text area
+                x = cX - 60  # Adjust as needed
+                y = cY + 40  # Adjust as needed
+                w = 100      # Width of the text area
+
+            # Calculate the area of the contour
+
+            area = cv2.contourArea(contour)
+            if 40000 > area > 30000:
+                # Put the area text on the frame, positioned near the bottom right of the contour's bounding rectangle
+                cv2.putText(frame, "Area: " + str(int(area)), (x + w - 60, y + 45), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0), 1)
+            if len(approxed)==3:    
+                cv2.putText(frame, "Points: " + str(len(approxed)), (x + w - 60, y + 60), cv2.FONT_HERSHEY_COMPLEX, 0.5,(0, 255, 0), 1)
+    
+# Loop through each contour in the second set of contours
+        for contour2 in contours2:
+            approx = cv2.approxPolyDP(contour2, 0.01 * cv2.arcLength(contour, True), True)
+
+            M = cv2.moments(approx)
+            if M["m00"] != 0:
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+
+                # Define position and size of the text area
+                x = cX - 50  # Adjust as needed
+                y = cY + 30  # Adjust as needed
+                w = 100      # Width of the text area
+            # Calculate the area of the contour
+            area = cv2.contourArea(contour2)
+            
+            # Put the area text on the frame, positioned near the bottom right of the contour's bounding rectangle
+            cv2.putText(frame, "Area: " + str(int(area)), (x + w - 60, y + 45), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0), 1)
+
+        for contour in contours:
+            epsilon = 0.03 * cv2.arcLength(contour, True)
+            approx = cv2.approxPolyDP(contour, epsilon, True)
+            cv2.drawContours(frame, [approx], -1, (255, 0, 0), 2)
+        cv2.drawContours(frame,contours2,-1,(255,0,0),10)
+      
+
+    
+        
+        # cv2.drawContours(frame_copy,contours2,-1,(255,0,0),10)
+        # combined_frame = np.hstack((frame,frame_copy))
+        
+        threshold_distance = 15
     # Adjust this value according to your requirement
 
         # Check if the frame is not empty
@@ -113,20 +168,35 @@ def video_feed(request, pk):
                 cv2.putText(frame, "PLAY", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)                
                 
                 
-                if not gameStarted: 
+                if not gameStarted:     #enters if condition if it is true so not gameStarted is true(! of false = true)
                     start_timer(request, pk)
-                    print('timerbdfbdfb started==========================')
+                    print('timer started==========================')
+                    frame
                     gameStarted=True
+                    count += 1
+                    print('count',count)
+
+            if gameStarted == True :
+                # detection(area,approxed)
+                if 40000 > area > 30000 and len(approxed)==3: 
+
+                    print('*******game end*******')
+                    gameStarted=False
+
+
                     
             # if masks_overlap(red_mask, mask):
             #     print("Play")
             # else:
             #     print("Game Over")
                 # cv2.putText(frame, "PLAY", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            
             ret, jpeg = cv2.imencode('.jpg', frame)
             frame = jpeg.tobytes()
             yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        
+         
             # cv2.imshow('blur', imgGray)
             # cv2.imshow('binary video',canny2)
 
